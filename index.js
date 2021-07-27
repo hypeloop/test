cab501e2ea9a14336ff639cbfd03fc76b263d72b
unknown <jvitor.sche@gmail.com>
2021-07-27 11:36:31 -0300

    test

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const socket = require('socket.io-client')('ws://0.0.0.0:3003')
 
 socket.on('connect', () => {
-  console.log('connected')
+  socket.emit('subscribe', 'agent')
+
+  socket.emit('update', {
+    manager: {
+      pid: 'asdasdad'
+    }
+  })
 })
